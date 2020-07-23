d53466faeb946174157902ffc38bd37111fc3c8f
Unix Aura <liridonsejdiu88@gmail.com>
2020-07-23 15:42:28 +0200

    add todo all

--- per-ne/todo.js
+++ per-ne/todo.js
@@ -1,5 +1,5 @@
 // katet
-// ? Manuali per kataster     nr objekti 72217082
-// ! nr i objektit 386
-// ! permirso bodrumin depo me shkall muri
-// ! shkallet ne perdhese hyrja 2
+// // Manuali per kataster     nr objekti 72217082
+//// nr i objektit 386
+//// permirso bodrumin depo me shkall muri
+//// shkallet ne perdhese hyrja 2
